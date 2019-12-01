a57791c298d39ab8e366576dfd6a1e37c8b99b7e
cathaldaly1 <16208102@studentmail.ul.ie>
2019-12-01 21:52:21 +0000

    Code for James

--- src/main/java/ui/controller/ViewTransactionsController.java
+++ src/main/java/ui/controller/ViewTransactionsController.java
@@ -1,34 +1,23 @@
 package ui.controller;
 
+import Auctions.Advert;
 import ui.view.ViewTransactionsScreen;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 public class ViewTransactionsController extends  BaseFrameController{
 
    private ViewTransactionsScreen viewTrans;
     private JButton review;
 
-    public ViewTransactionsController() {
-
-    }
+    public ViewTransactionsController() { }
 
     public void controlTransactions() {
 
         viewTrans = new ViewTransactionsScreen();
         review = viewTrans.getReview();
-        addListeners();
         viewTrans.setVisible(true);
-    }
-
-    public void addListeners() {
-
-        review.addActionListener((ActionEvent e) -> {
-            System.out.println("IMPLEMENT - review");
-            review.setVisible(false);
-            //ReviewSellerController rev = new ReviewSellerController();
-            //rev.controlReviewSeller();
-        });
     }
 }
