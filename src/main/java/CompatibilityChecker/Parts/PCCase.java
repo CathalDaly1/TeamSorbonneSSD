c1fabf99d4defa9ad03617324dd8b4fe3c826c20
Cathal Daly <32312784+CathalDaly1@users.noreply.github.com>
2020-05-13 13:54:09 +0100

    Adapter DP cdogg

---
+++ src/main/java/CompatibilityChecker/Parts/PCCase.java
@@ -0,0 +1,63 @@
+package CompatibilityChecker.Parts;
+
+import CompatibilityChecker.Configuration.ConfigurationDetails;
+
+public class PCCase extends unsupportedPart {
+
+    private String caseObj;
+    private double height;
+    private double width;
+    private String colour;
+
+    public PCCase(String name, String brand, double wattage, String caseObj, double height, double width, String colour) {
+        super(name, brand, wattage);
+        this.caseObj = caseObj;
+        this.height = height;
+        this.width = width;
+        this.colour = colour;
+    }
+
+    public String getCaseObj() {
+        return caseObj;
+    }
+
+    public void setCaseObj(String caseObj) {
+        this.caseObj = caseObj;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double dataRate) {
+        this.width = width;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+     @Override
+    public ConfigurationDetails getNullConfigDetails(ConfigurationDetails configurationDetails) {
+        System.out.println("Case GetConfig called");
+        configurationDetails.setWattageIn(configurationDetails.getWattageIn()+this.getWattage());
+        return configurationDetails;
+    }
+
+    @Override
+    public ConfigurationDetails getConfiguration(ConfigurationDetails configurationDetails) {
+        return null;
+    }
+}
